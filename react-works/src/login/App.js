75a27a83c6c8edf99ea523a93851fb07e4fe682c
bonomiandreia <bonomiandreia@gmail.com>
2021-11-22 19:54:51 -0300

    login screen

--- react-works/src/login/App.js
+++ react-works/src/login/App.js
@@ -1,9 +1,40 @@
 import './App.scss';
+import Card from '@mui/material/Card';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
-    <div className="App">
-      start a react project!
+    <div className="c-login">
+      <div className="c-login__position">
+        <h1>Pet Diary</h1>
+        <Card sx={{ p: '2% 2%', display: 'flex', alignItems: 'center', width: 600 }}>
+
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <h2>Welcome!</h2>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              id="outlined-name"
+              label="email"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              id="outlined-name"
+              label="password"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button variant="contained" size="large" fullWidth>LOGIN IN</Button>
+          </Grid>
+        </Grid>
+        </Card>
+      </div>
     </div>
   );
 }
