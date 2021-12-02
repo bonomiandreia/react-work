04a8aebc3b7ece41a545d3fa4a4a3e9adabaf89d
bonomiandreia <bonomiandreia@gmail.com>
2021-12-02 11:31:50 -0300

    responsive

--- react-works/src/login/App.js
+++ react-works/src/login/App.js
@@ -9,34 +9,30 @@ function App() {
     <div className="c-login">
       <div className="c-login__position">
         <h1>Pet Diary</h1>
-        <Card sx={{ p: '2% 2%', m: '3% 3%', display: 'flex', alignItems: 'center', width: {
-          sx: 100,
-          sm: 300,
-          md: 600,
-        }}}>
+        <Card sx={{ p: '10% 10%', m: '3% 3%', display: 'flex', alignItems: 'center'}}>
 
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <h2>Welcome!</h2>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <h2>Welcome!</h2>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                id="outlined-name"
+                label="email"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                id="outlined-name"
+                label="password"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button variant="contained" size="large" fullWidth>LOGIN IN</Button>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <TextField
-              id="outlined-name"
-              label="email"
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              id="outlined-name"
-              label="password"
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button variant="contained" size="large" fullWidth>LOGIN IN</Button>
-          </Grid>
-        </Grid>
         </Card>
       </div>
     </div>
