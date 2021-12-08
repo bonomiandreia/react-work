adc54d19ca0c9349a8111492f17bccb8a8002947
bonomiandreia <bonomiandreia@gmail.com>
2021-12-08 11:53:55 -0300

    input

--- react-works/src/components/Gray-input/Gray-input.js
+++ react-works/src/components/Gray-input/Gray-input.js
@@ -1,18 +1,19 @@
-import * as React from 'react';
 import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles(theme => ({
-  input: props => ({
-      backgroundColor: props.backgroundColor,
-      '& label.Mui-focused': {
-        color: '#1F9EBC',
-     },
-     '& .MuiOutlinedInput-root': {
-        '&.Mui-focused fieldset': {
-           borderColor: '#1F9EBC',
-        },
-     },
-  }),
+   input: props => ({
+         backgroundColor: props.backgroundColor,
+         borderRadius: 10,
+         '& label.Mui-focused': {
+            color: '#1F9EBC',
+         },
+         '& .MuiOutlinedInput-root': {
+            borderRadius: 10,
+            '&.Mui-focused fieldset': {
+               borderColor: '#1F9EBC',
+         },
+      },
+   }),
 }));
 
 export default useStyles;
